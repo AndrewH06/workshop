e5ca8a55a12357f32da476a4638659e1b1bd9ce6
AndrewH06 <andrew.z.hale1@gmail.com>
2024-04-10 18:47:34 -0700

    🍓 Finished strawberry function, closes: #1

--- fruits.py
+++ fruits.py
@@ -1,8 +1,14 @@
 def apple():
     print("I am an apple")
 
+def orange():
+    print("I am an orange")
+
 def banana():
     print("I am a banana")
 
 def potato():
     print("I am a potato")
+
+def strawberry():
+    print("I am a strawberry")
